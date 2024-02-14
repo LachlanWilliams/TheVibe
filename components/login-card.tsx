d37423856d33f6da3b22ebdb435b3074a5a6343c
Lachlan Williams <lwil0039@student.monash.edu>
2024-02-14 16:12:49 +1100

    login card has contents of product card

---
+++ components/login-card.tsx
@@ -0,0 +1,46 @@
+import { Image } from 'expo-image';
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { Card, H2, Paragraph } from 'tamagui';
+
+import HeartButton from './heart-button';
+
+export default function customProductCard({
+  imageLink,
+  name,
+  price,
+}: {
+  imageLink: string;
+  name: string;
+  price: number;
+}) {
+  return (
+    <Card elevate size="$4" height={500}>
+      <Card.Header padded>
+        <H2>{name}</H2>
+        <Paragraph theme="alt2">${price}</Paragraph>
+      </Card.Header>
+      <Card.Footer>
+        <HeartButton />
+      </Card.Footer>
+      <Card.Background>
+        <Image style={styles.image} source={imageLink} />
+      </Card.Background>
+    </Card>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#0553',
+  },
+});
