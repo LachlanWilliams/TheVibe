d96b73fe00cbc080bdd3436e8ae342e393b1b8cf
Lachlan Williams <lwil0039@student.monash.edu>
2024-02-22 11:34:28 +1100

    login modal set false

--- components/login-card.tsx
+++ components/login-card.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
-import { Image, StyleSheet } from 'react-native';
-import { Card, H2, Button, Input, YStack } from 'tamagui';
+import { Image, Modal, StyleSheet, SafeAreaView } from 'react-native';
+import { Card, H2, Button, Input, YStack, View, Text } from 'tamagui';
 import { router } from 'expo-router'; // Assuming you have imported the router instance
 
 export default function LoginCard() {
+  const [modalOpen, setModalOpen] = useState(false);
+
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
@@ -15,35 +17,42 @@ export default function LoginCard() {
   };
 
   return (
-    <Card elevate size="$4" height={700} width={350}>
-      <Card.Header padded>
-        <H2>The Vibe</H2>
-      </Card.Header>
-      <Card.Footer>
-        <YStack>
-          <Input
-            placeholder="User Name"
-            value={username}
-            onChangeText={(text) => setUsername(text)}
+    <>
+      <Modal visible={modalOpen}>
+        <SafeAreaView>
+          <Text>Hello</Text>
+        </SafeAreaView>
+      </Modal>
+      <Card elevate size="$4" height={700} width={350}>
+        <Card.Header padded>
+          <H2>The Vibe</H2>
+        </Card.Header>
+        <Card.Footer>
+          <YStack>
+            <Input
+              placeholder="User Name"
+              value={username}
+              onChangeText={(text) => setUsername(text)}
+            />
+            <Input
+              placeholder="Password"
+              value={password}
+              onChangeText={(text) => setPassword(text)}
+              secureTextEntry={true} // For password input
+            />
+            <Button onPress={handleSignIn}>Sign in</Button>
+          </YStack>
+        </Card.Footer>
+        <Card.Background>
+          <Image
+            style={styles.image}
+            source={{
+              uri: 'https://i.pinimg.com/originals/db/69/09/db69093d2a2f46b53e88f4e3f54f0691.jpg',
+            }}
           />
-          <Input
-            placeholder="Password"
-            value={password}
-            onChangeText={(text) => setPassword(text)}
-            secureTextEntry={true} // For password input
-          />
-          <Button onPress={handleSignIn}>Sign in</Button>
-        </YStack>
-      </Card.Footer>
-      <Card.Background>
-        <Image
-          style={styles.image}
-          source={{
-            uri: 'https://i.pinimg.com/originals/db/69/09/db69093d2a2f46b53e88f4e3f54f0691.jpg',
-          }}
-        />
-      </Card.Background>
-    </Card>
+        </Card.Background>
+      </Card>
+    </>
   );
 }
 
