aed20976292b0cf0ff9b064357d45b68eb396ac0
Lachlan Williams <lwil0039@student.monash.edu>
2024-02-28 15:41:35 +1100

    signup page console logs

--- app/(login)/signup.tsx
+++ app/(login)/signup.tsx
@@ -1,10 +1,17 @@
-import { YStack, H2, Theme, Input } from 'tamagui';
+import { YStack, H2, Theme, Input, Button } from 'tamagui';
 
 export default function SignUp() {
+  const handleSignUp = () => {
+    console.log('Creating Account');
+  };
   return (
     <Theme name="light">
       <YStack flex={1} alignItems="center" justifyContent="center">
         <H2>Create a new account</H2>
+        <Input placeholder="Email" />
+        <Input placeholder="Confirm Email" />
+        <Input placeholder="password" />
+        <Button onPress={handleSignUp}>Create Account</Button>
       </YStack>
     </Theme>
   );
