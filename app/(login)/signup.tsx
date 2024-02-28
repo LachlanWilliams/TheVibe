7181fe20da20ce9902f4b9a74035de17ca33d420
Lachlan Williams <lwil0039@student.monash.edu>
2024-02-28 15:59:30 +1100

    signup page input handling

--- app/(login)/signup.tsx
+++ app/(login)/signup.tsx
@@ -1,16 +1,40 @@
+import { router } from 'expo-router';
+import { useState } from 'react';
 import { YStack, H2, Theme, Input, Button } from 'tamagui';
 
 export default function SignUp() {
+  const [email, setEmail] = useState('');
+  const [confirmEmail, setConfirmEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   const handleSignUp = () => {
-    console.log('Creating Account');
+    if (email === '' || confirmEmail === '' || password === '') {
+      alert('Fill out all the information');
+    } else if (email !== confirmEmail) {
+      alert('Ensure both emails are the same');
+    } else {
+      console.log('Creating Account');
+    }
+    console.log('\nEmail: ', email, '\nConfirmEmail: ', confirmEmail, '\nPassword: ', password);
+    //router.navigate('(login)');
   };
+
   return (
     <Theme name="light">
       <YStack flex={1} alignItems="center" justifyContent="center">
         <H2>Create a new account</H2>
-        <Input placeholder="Email" />
-        <Input placeholder="Confirm Email" />
-        <Input placeholder="password" />
+        <Input placeholder="Email" value={email} onChangeText={(text) => setEmail(text)} />
+        <Input
+          placeholder="Confirm Email"
+          value={confirmEmail}
+          onChangeText={(text) => setConfirmEmail(text)}
+        />
+        <Input
+          placeholder="password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          secureTextEntry={true}
+        />
         <Button onPress={handleSignUp}>Create Account</Button>
       </YStack>
     </Theme>
