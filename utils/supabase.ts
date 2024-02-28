191274662c50b99033403cf8f80b6f0c27287c1c
Lachlan Williams <lwil0039@student.monash.edu>
2024-02-29 10:32:19 +1100

    stop export of supabase client object

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -11,7 +11,7 @@ export interface Product {
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     storage: AsyncStorage,
     autoRefreshToken: true,
